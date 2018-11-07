4afcf35fecdf9a920eef9bd3b4a4af8acec990b5
F3Bots <44725715+F3Bots@users.noreply.github.com>
2018-11-07 06:22:53 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,5 @@
 
 const Discord = require('discord.js');
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`*Fuck OFF`,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
-
-
 const Util = require('discord.js');
 const getYoutubeID = require('get-youtube-id');
 
